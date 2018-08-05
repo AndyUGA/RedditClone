e517e9c2ec17123f0fed4c20a4fad0bfca00ad44
Andy Truong <att35663@uga.edu>
2018-08-05 13:57:41 -0400

    Added thumbnails and modified font

--- script.js
+++ script.js
@@ -16,6 +16,11 @@
 
     app.controller('myCtrl', function($scope, $http) {
         
+        $http.get("https://www.reddit.com/r/all/hot/.json?limit=20")
+            .then(function(myArr) {
+            console.log(myArr);
+            $scope.responseData = myArr;
+        })
         
         $scope.call = function() {
             $http.get("https://www.reddit.com/r/" + $scope.subReddit + "/hot/.json?limit=20")

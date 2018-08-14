cde8878ae90fddce89b99b5bd3221a68abbedc90
Andy <att35663@uga.edu>
2018-08-14 16:07:08 -0400

    Modified variable names to be more appropriate and commented code

--- script.js
+++ script.js
@@ -17,7 +17,9 @@
     app.controller('myCtrl', function($scope, $http) {
         
         $scope.names = ["hot", "top", "new", "rising"];
-        $scope.userOption = "hot";
+        $scope.redditFilter = "hot";
+       
+        //Gets data from all subreddit to display on home page
         $http.get("https://www.reddit.com/r/all/hot/.json?limit=20")
             .then(function(myArr) {
 
@@ -25,34 +27,16 @@
             $scope.responseData = myArr;
         })
         
-        $scope.Hot = function() {
-            $http.get("https://www.reddit.com/r/" + $scope.subReddit + "/" + $scope.userOption + "/.json?limit=20")
+        //Gets data from user specified subreddit and filter 
+        $scope.search = function() {
+            $http.get("https://www.reddit.com/r/" + $scope.subReddit + "/" + $scope.redditFilter + "/.json?limit=20")
             .then(function(myArr) {
             console.log(myArr);
             $scope.responseData = myArr;
         });
         }
         
-        $scope.Top = function() {
-            $http.get("https://www.reddit.com/r/" + $scope.subReddit + "/top/.json?limit=20")
-            .then(function(myArr) {
-            $scope.responseData = myArr;
-        });
-        }
         
-        $scope.New = function() {
-            $http.get("https://www.reddit.com/r/" + $scope.subReddit + "/new/.json?limit=20")
-            .then(function(myArr) {
-            $scope.responseData = myArr;
-        });
-        }
-        
-        $scope.Rising = function() {
-            $http.get("https://www.reddit.com/r/" + $scope.subReddit + "/rising/.json?limit=20")
-            .then(function(myArr) {
-            $scope.responseData = myArr;
-        });
-        }
         
         
         

3ae6e5f2b156f27a448c890d6bfa5a9cd9f942d8
Andy <att35663@uga.edu>
2018-08-20 23:46:02 -0400

    Added UGA subreddit to menu bar

--- script.js
+++ script.js
@@ -29,7 +29,7 @@
         $scope.search = function() {
             $http.get("https://www.reddit.com/r/" + $scope.subReddit + "/" + $scope.redditFilter + "/.json?limit=20")
             .then(function(myArr) {
-            console.log(myArr);
+
             $scope.responseData = myArr;
         });
         }
@@ -37,7 +37,7 @@
         $scope.searchAll = function() {
             $http.get("https://www.reddit.com/r/all/" + $scope.redditFilter + "/.json?limit=20")
             .then(function(myArr) {
-            console.log(myArr);
+
             $scope.responseData = myArr;
         });
         }
@@ -45,7 +45,7 @@
         $scope.searchOC = function() {
             $http.get("https://www.reddit.com/original/" + $scope.redditFilter + "/.json?limit=20")
             .then(function(myArr) {
-            console.log(myArr);
+
             $scope.responseData = myArr;
         });
         }
@@ -53,11 +53,18 @@
         $scope.searchApple = function() {
             $http.get("https://www.reddit.com/r/apple/" + $scope.redditFilter + "/.json?limit=20")
             .then(function(myArr) {
-            console.log(myArr);
+
             $scope.responseData = myArr;
         });
         }
         
+        $scope.searchUGA = function() {
+            $http.get("https://www.reddit.com/r/UGA/" + $scope.redditFilter + "/.json?limit=20")
+            .then(function(myArr) {
+
+            $scope.responseData = myArr;
+        });
+        }
         
         
         

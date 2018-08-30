d263d6439328dcdb87519a9ee9f69bf9e5577132
Andy <att35663@uga.edu>
2018-08-30 08:35:49 -0400

    Added comments to script page

--- script.js
+++ script.js
@@ -10,13 +10,12 @@
     var app = angular.module('myApp', ['ngMaterial', 'ngMessages']);
    
     
-    
-    
-
-
     app.controller('myCtrl', function($scope, $http) {
         
+        //Filter options 
         $scope.names = ["hot", "top", "new", "rising"];
+        
+        //Set default filter to hot
         $scope.redditFilter = "hot";
        
         //Gets data from all subreddit to display on home page
@@ -29,12 +28,15 @@
         $scope.search = function() {
             $http.get("https://www.reddit.com/r/" + $scope.subReddit + "/" + $scope.redditFilter + "/.json?limit=20")
             .then(function(myArr) {
-
+                
             $scope.responseData = myArr;
+                
+            //Reset search filter query 
             $scope.subReddit = "";
         });
         }
         
+
         $scope.searchAll = function() {
             $http.get("https://www.reddit.com/r/all/" + $scope.redditFilter + "/.json?limit=20")
             .then(function(myArr) {
